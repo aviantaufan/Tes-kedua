8237ad3b656e10dec75677c2cf62bf492401cc25
aviantaufan <aviantaufan@gmail.com>
2026-07-12 17:37:22 +0000

    Memperbaiki koma pada objek stats

--- src/components/CinemaReel.tsx
+++ src/components/CinemaReel.tsx
@@ -51,8 +51,8 @@ const SLIDES: Slide[] = [
   director: "Tukang Interior Batam",
   imageId: "https://imagizer.imageshack.com/img924/4595/IvPnzu.png",
   awards: [],
-  description: "Desain yang baik tidak sekadar indah dipandang, tetapi juga harus membawa ketenangan bagi penghuninya. Instalasi panel dinding bertekstur rapi, pencahayaan ruang yang terukur, dan detail pengerjaan yang halus. merupakan elemen kunci untuk menciptakan nuansa modern luxury yang tak lekang oleh waktu.merencanakan visualisasi desain adalah langkah pertama yang krusial sebelum memulai renovasi. Bagi Anda yang berencana memperbarui tampilan ruang keluarga, kamar, atau area komersial di Batam dan sekitarnya, kami hadir untuk membantu mewujudkan konsep tersebut dengan nyata. Mari mulai obrolan santai tentang ruang impian Anda."
-  stats: { critics: 94, audience: 81, runtime: "1h 38m", format: "16MM", country: "UK · GERMANY", language: "ENGLISH", aspect: "1.66:1" },
+  description: "Desain yang baik tidak sekadar indah dipandang, tetapi juga harus membawa ketenangan bagi penghuninya. Instalasi panel dinding bertekstur rapi, pencahayaan ruang yang terukur, dan detail pengerjaan yang halus. merupakan elemen kunci untuk menciptakan nuansa modern luxury yang tak lekang oleh waktu.merencanakan visualisasi desain adalah langkah pertama yang krusial sebelum memulai renovasi. Bagi Anda yang berencana memperbarui tampilan ruang keluarga, kamar, atau area komersial di Batam dan sekitarnya, kami hadir untuk membantu mewujudkan konsep tersebut dengan nyata. Mari mulai obrolan santai tentang ruang impian Anda.",
+  stats: { critics: "94", audience: "81", runtime: "1h 38m", format: "16MM", country: "UK · GERMANY", language: "ENGLISH", aspect: "1.66:1" },
 },
   {
     id: "atlas-unfolds",
